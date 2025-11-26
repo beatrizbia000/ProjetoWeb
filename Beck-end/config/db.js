e5d29b7a471ab7c0a9296c52516ba01ece270609
juliocesar0707 <juliocmoraisdsilva7@gmail.com>
2025-11-26 19:32:24 -0300

    Fix: Adicionando porta e SSL no db.js

--- Beck-end/config/db.js
+++ Beck-end/config/db.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = new mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 3306, 
+  ssl: {
+      rejectUnauthorized: false 
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
